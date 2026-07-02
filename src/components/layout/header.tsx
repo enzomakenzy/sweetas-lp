967ebedb64016afb62ff28502b01b8e70ef39c0c
enzomakenzy <enzomqueiroz@gmail.com>
2026-07-02 14:43:35 -0300

    style: altered white main color and logo header height

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,12 +3,12 @@ import { Button } from "../ui/button";
 
 export function Header() {
   return (
-    <header className="grid grid-cols-2 py-3 px-4 lg:grid-cols-3 lg:px-20 xl:px-30 2xl:px-40 border-b items-center ">
+    <header className="grid grid-cols-2 py-3 px-4 sm:px-14 lg:grid-cols-3 lg:px-20 xl:px-30 2xl:px-40 border-b items-center">
       <a href="#">
         <img 
           src={logo}
           alt="Logo SweetA's"
-          className="w-30 lg:w-38"
+          className="w-30 md:w-34 lg:w-38"
         />
       </a>
 
