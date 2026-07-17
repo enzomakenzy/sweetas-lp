86681654082c7af57e09d4f1f87433fa34564345
enzomakenzy <enzomqueiroz@gmail.com>
2026-07-17 17:03:23 -0300

    refactor: update paddings, logo size and navigation link styles

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,19 +3,19 @@ import { Button } from "../ui/button";
 
 export function Header() {
   return (
-    <header className="grid grid-cols-2 py-3 px-4 sm:px-14 lg:grid-cols-3 lg:px-20 xl:px-30 2xl:px-40 border-b items-center absolute bg-background w-full">
+    <header className="grid grid-cols-2 py-6 px-4 sm:px-14 lg:grid-cols-3 lg:px-20 xl:px-30 2xl:px-40 items-center absolute bg-background w-full">
       <a href="#">
         <img 
           src={logo}
           alt="Logo SweetA's"
-          className="w-30 md:w-34 lg:w-38"
+          className="w-30 md:w-34 lg:w-44"
         />
       </a>
 
       <nav className="hidden lg:flex lg:items-center lg:justify-center lg:gap-12">
-        <a className="font-semibold text-terciary hover:text-accent text-base" href="#">Cardápio</a>
-        <a className="font-semibold text-terciary hover:text-accent text-base" href="#">Qualidade</a>
-        <a className="font-semibold text-terciary hover:text-accent text-base" href="#">Contato</a>
+        <a className="font-semibold text-primary hover:text-accent text-xl" href="#">Cardápio</a>
+        <a className="font-semibold text-primary hover:text-accent text-xl" href="#">Qualidade</a>
+        <a className="font-semibold text-primary hover:text-accent text-xl" href="#">Contato</a>
       </nav>
 
       <div className="flex justify-end">
