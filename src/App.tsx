283695372fcfdbcfda03c5a1f5d9f86768b843d4
enzomakenzy <enzomqueiroz@gmail.com>
2026-07-30 21:40:22 -0300

    feat: add top 3 flavors section with ranking cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/layout/header";
 import { Banner } from "./components/sections/banner";
 import { Marquee } from "./components/sections/marquee";
+import { Top3 } from "./components/sections/top-3";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <div>
         <Banner />
         <Marquee />
+        <Top3 />
       </div>
     </div>
   );
---
+++ src/components/common/top-3-card.tsx
@@ -0,0 +1,56 @@
+import { Heart, Star } from "lucide-react";
+
+type Props = {
+  position: number;
+  favlorName: string;
+  description: string;
+  image: string;
+  imageAlt: string;
+  className: string
+};
+
+function listStars(position: number) {
+  const stars = [];
+  
+  for (let i = 1; i <= 5; i++) {
+    stars.push(
+      <Star className={`w-2.5 z-10 ${position === 1 ? "text-background fill-background" : "text-tertiary fill-tertiary"}`} />
+    )
+  }
+
+  return stars
+}
+
+export function Top3Card({ position, favlorName, description, image, imageAlt, className }: Props) {
+  return (
+    <div className={`${position === 1 ? "bg-primary" : "bg-background"} rounded-4xl px-5 py-6 flex flex-col gap-5 shadow relative overflow-hidden ${className}`}>
+      <p className={`${position === 1 ? "text-background/20" : "text-primary/20"} absolute -right-5 -top-16 text-[142px] font-bold z-0`}>{position}</p>
+
+      <div className="flex items-center justify-between">
+        <p className={`${position === 1 ? "bg-background" : "bg-tertiary/12"} text-primary px-3 py-1 text-xs rounded-4xl font-bold`}>{position === 1 && "👑"} # {position}</p>
+
+        <div className="flex gap-1">
+          {listStars(position)}
+        </div>
+      </div>
+
+      <div className="relative">
+        <img src={image} alt={imageAlt} className="rounded-2xl w-full" />
+
+        {position === 1 && (
+          <div className="absolute bottom-3 left-3 right-3 flex items-center justify-between bg-background px-3 py-1 rounded-4xl">
+            <p className="uppercase font-bold text-xs text-primary tracking-[0.05rem]">Mais pedido</p>
+
+            <Heart className="fill-tertiary text-tertiary w-3 h-3" />
+          </div>
+        )}
+      </div>
+
+      <div className="pb-1">
+        <h4 className={`text-2xl font-heading font-bold uppercase ${position === 1 ? "text-background" : "text-primary"}`}>{favlorName}</h4>
+
+        <h4 className={`text-2xl/5 font-accent ${position === 1 ? "text-background" : "text-secondary"}`}>{description}</h4>
+      </div>
+    </div>
+  );
+}
