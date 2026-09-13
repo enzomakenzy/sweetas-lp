160d737830783f69f4d73d7cb3c24c6a153302cb
enzomakenzy <enzomqueiroz@gmail.com>
2026-09-12 22:28:53 -0300

    feat: make header fixed and add anchor navigation

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,7 +3,7 @@ import { Button } from "../ui/button";
 
 export function Header() {
   return (
-    <header className="grid grid-cols-2 py-6 px-4 lg:grid-cols-3 lg:px-20 xl:px-30 2xl:px-40 items-center absolute bg-background w-full">
+    <header className="grid grid-cols-2 py-3 px-4 lg:grid-cols-3 lg:px-20 xl:px-30 2xl:px-40 items-center top-0 fixed z-30 bg-background w-full">
       <a href="#">
         <img 
           src={logo}
@@ -12,14 +12,16 @@ export function Header() {
         />
       </a>
 
-      <nav className="hidden lg:flex lg:items-center lg:justify-center lg:gap-12">
-        <a className="font-semibold text-primary hover:text-accent text-xl" href="#">Cardápio</a>
-        <a className="font-semibold text-primary hover:text-accent text-xl" href="#">Qualidade</a>
-        <a className="font-semibold text-primary hover:text-accent text-xl" href="#">Contato</a>
+      <nav className="hidden lg:flex lg:items-center lg:justify-center lg:gap-12 *:font-semibold *:text-primary *:hover:text-accent *:text-xl">
+        <a href="/#inicio">Início</a>
+        <a href="/#cardapio">Cardápio</a>
+        <a href="/#sobre">Sobre</a>
       </nav>
 
       <div className="flex justify-end">
-        <Button size="default">Peça já</Button>
+        <a href="/#peca-ja">
+          <Button size="default">Peça já</Button>
+        </a>
       </div>
     </header>
   );
