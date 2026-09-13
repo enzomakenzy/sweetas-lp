cb37d6f557a351ed598148f3d8fd53fb16405e33
enzomakenzy <enzomqueiroz@gmail.com>
2026-09-12 22:29:13 -0300

    style: refine card hover effects and add order button to menu card

--- src/components/common/menu-card.tsx
+++ src/components/common/menu-card.tsx
@@ -1,4 +1,5 @@
 import { Diamond } from "lucide-react"
+import { Button } from "../ui/button";
 
 type Props = {
   favlorName: string;
@@ -10,18 +11,22 @@ type Props = {
 
 export function MenuCard({ favlorName, price, description, image, imageAlt }: Props) {
   return (
-    <div className="rounded-2xl overflow-hidden border-2 border-quaternary/50 relative transition-all duration-400 lg:hover:shadow-2xl lg:max-w-full bg-card lg:hover:border-primary lg:hover:-translate-y-2 group">
-      <img src={image} alt={imageAlt} className="h-72 xs:h-96 sm:h-72 2md:h-96 w-full object-cover object-top transition duration-400 group-hover:lg:scale-104 group-hover:lg:-translate-y-2" />
+    <div className="rounded-2xl overflow-hidden border-2 border-quaternary/50 relative transition-all duration-400 lg:hover:shadow-2xl lg:max-w-full bg-card lg:hover:border-primary lg:hover:-translate-y-2 group h-full flex flex-col">
+      <img src={image} alt={imageAlt} className="h-72 xs:h-96 sm:h-72 2md:h-96 w-full object-cover object-top transition duration-400 group-hover:lg:scale-105 group-hover:lg:-translate-y-2" />
 
-      <div className="p-5">
+      <div className="p-5 flex flex-col flex-1">
         <h3 className="font-heading font-bold text-primary text-xl">{favlorName}</h3> 
+        <p className="text-quaternary text-sm text-justify pt-1 pb-3">{description}</p>
 
-        <span className="flex gap-2 items-center w-18.5 my-1">
-          <Diamond className="w-3.5 fill-secondary text-secondary" />
-          <p className="font-bold text-lg text-quaternary">R$ {price}</p>
-        </span>  
+        <div className="flex items-center justify-between mt-auto">
+          <p className="flex items-center gap-2 font-bold text-lg text-quaternary">
+            <Diamond className="w-3.5 fill-secondary text-secondary" />R$ {price}
+          </p>
 
-        <p className="text-quaternary text-sm text-justify">{description}</p>
+          <a href="#">
+            <Button className="uppercase" size="sm">Fazer pedido</Button>
+          </a>
+        </div>  
       </div>
     </div>
   )
--- src/components/common/review-card.tsx
+++ src/components/common/review-card.tsx
@@ -8,14 +8,14 @@ interface Props {
 
 export function ReviewCard({ review, author, reviewCategory }: Props) {
   return (
-    <div className="relative px-7 py-6 bg-card rounded-2xl border-2 border-quinary shadow text-start transition duration-250 hover:border-primary/50 hover:-translate-y-1.5 hover:shadow-lg grid content-between">
+    <div className="relative px-7 py-6 bg-card rounded-2xl border-2 border-quinary shadow text-start transition duration-250 hover:border-primary/50 hover:-translate-y-1.5 hover:shadow-lg grid content-between group">
       <div>
         <div className="mb-3">
           <div className="flex gap-1">
             {listStars("RED")}
           </div>
 
-          <span className="absolute top-4 right-7 text-6xl font-accent leading-none text-secondary">”</span>
+          <span className="absolute top-4 transition duration-250 right-7 text-6xl font-accent leading-none text-secondary group-hover:text-tertiary">”</span>
         </div>
 
         <p className="text-quaternary font-medium mb-4">“{review}”</p>
