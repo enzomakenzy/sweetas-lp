915815015a5fc128e603b36814ce7aaf93c5e877
enzomakenzy <enzomqueiroz@gmail.com>
2026-09-14 17:29:12 -0300

    feat: add mobile hamburger menu and auto-hide on scroll

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,28 +1,76 @@
-import logo from "@/assets/images/main-logo.svg" 
+import logo from "@/assets/images/main-logo.svg";
+import navButton from "@/assets/images/nav-button.svg";
 import { Button } from "../ui/button";
 import { NavLink } from "react-router";
+import { useEffect, useRef, useState } from "react";
 
 export function Header() {
+  const [menuStatus, setMenuStatus] = useState<boolean>(false); 
+  const [hidden, setHidden] = useState(false);
+  const lastScrollY = useRef(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      // evita esconder logo no topo da página
+      if (currentScrollY < 80) {
+        setHidden(false);
+      } else if (currentScrollY > lastScrollY.current) {
+        setHidden(true); // rolando pra baixo
+      } else {
+        setHidden(false); // rolando pra cima
+      }
+
+      lastScrollY.current = currentScrollY;
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
   return (
-    <header className="grid grid-cols-2 py-3 px-4 lg:grid-cols-3 lg:px-20 xl:px-30 2xl:px-40 items-center top-0 sticky z-30 bg-background w-full">
-      <a href="#">
-        <img 
-          src={logo}
-          alt="Logo SweetA's"
-          className="w-30 md:w-34 lg:w-44"
-        />
-      </a>
-
-      <nav className="hidden lg:flex lg:items-center lg:justify-center lg:gap-12 *:font-semibold *:text-primary *:hover:text-accent *:text-xl">
-        <NavLink to="/">Início</NavLink>
-        <NavLink to="/#cardapio">Cardápio</NavLink>
-        <NavLink to="/sobre">Sobre</NavLink>
-      </nav>
-
-      <div className="flex justify-end">
-        <NavLink to="/pedido">
-          <Button size="default">Peça já</Button>
+    <header className={`transition duration-300 top-0 sticky z-30 bg-background w-full h-18 ${hidden ? "-translate-y-full" : "translate-y-0"}`}>
+      <div className="relative py-3 h-full w-full flex justify-end items-center md:justify-between">
+        <NavLink to="/" className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 md:left-0 md:translate-0 md:top-0 md:static md:pl-12">
+          <img 
+            src={logo}
+            alt="Logo SweetA's"
+            className="w-38 md:w-42 lg:w-46"
+          />
         </NavLink>
+        
+
+        <nav className="hidden md:flex md:items-center md:font-semibold md:justify-center md:gap-12 *:text-primary *:hover:text-accent *:md:text-lg *:lg:text-xl">
+          <NavLink to="/">Início</NavLink>
+          <NavLink to="/#cardapio">Cardápio</NavLink>
+          <NavLink to="/sobre">Sobre</NavLink>
+        </nav>
+
+        <div className="hidden md:flex md:justify-end md:pr-12">
+          <NavLink to="/pedido">
+            <Button size="default">Peça já</Button>
+          </NavLink>
+        </div>
+
+        <button onClick={() => setMenuStatus(!menuStatus)} className="flex flex-col md:hidden cursor-pointer h-full p-2 mr-4 md:pr-10">
+          <img src={navButton} alt="" className="w-6" />
+        </button>
+
+      { menuStatus &&
+        <div className="absolute bg-background top-full w-full p-4 pb-7">
+          <nav className="flex flex-col items-center font-semibold justify-center gap-6 *:text-primary *:hover:text-accent *:text-xl *:w-full *:text-center lg:hidden">
+            <NavLink onClick={() => setMenuStatus(false)} to="/">Início</NavLink>
+            <NavLink onClick={() => setMenuStatus(false)} to="/#cardapio">Cardápio</NavLink>
+            <NavLink onClick={() => setMenuStatus(false)} to="/sobre">Sobre</NavLink>
+            <NavLink onClick={() => setMenuStatus(false)} to="/pedido">
+              <Button size="lg" className="w-full text-lg">Peça já</Button>
+            </NavLink>
+          </nav>
+
+        </div>
+      }
       </div>
     </header>
   );
