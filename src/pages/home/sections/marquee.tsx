1b51877cbcedac013c200bbe202763c63b870257
enzomakenzy <enzomqueiroz@gmail.com>
2026-09-17 12:22:01 -0300

    style(marquee): update typography, slow down animation and adjust icon sizes

--- src/pages/home/sections/marquee.tsx
+++ src/pages/home/sections/marquee.tsx
@@ -3,8 +3,8 @@ import { Club, Diamond, Heart, Spade } from "lucide-react";
 export function Marquee() {
   return (
     <section className="overflow-hidden bg-primary w-full mt-14">
-      <div className="flex py-3 md:py-4 animate-marquee w-max font-heading text-lg md:text-xl tracking-widest">
-        <span className="uppercase flex gap-4 text-background whitespace-nowrap pr-4 items-center *:card">
+      <div className="flex py-4 md:py-5 animate-marquee w-max font-sans font-bold text-white text-2xl md:text-[26px] tracking-widest">
+        <span className="uppercase flex gap-4 whitespace-nowrap pr-4 items-center *:card">
           Brigadeiro<Heart />
           KitKat<Spade />
           Ouro Branco<Diamond />
