6e0d0613ad301cee80fafb349a98f9fce96dba66
enzomakenzy <enzomqueiroz@gmail.com>
2026-09-21 16:10:44 -0300

    style: refine top 3 circle component and adjust grid positioning

--- src/components/common/top-3-circle.tsx
+++ src/components/common/top-3-circle.tsx
@@ -10,17 +10,18 @@ interface Props {
 export function Top3Circle({ position, favlorName, description, image, imageAlt, className }: Props) {
   return (
     <div className={`relative rounded-full z-0 flex flex-col items-center ${className}`}>
-      <div className="absolute z-10 top-o right-0 bg-secondary w-18.5 h-18.5 rounded-full font-heading font-bold text-2xl flex justify-center items-center rotate-18 border-3 border-card">#0{position}</div>
 
-      <div className="relative w-80 h-80 flex justify-center items-center">
-        <div className="absolute inset-0 border-2 border-primary border-dashed rounded-full animate-ring-spin"></div>
+      <div className="relative flex justify-center items-center overflow-hidden max-w-100">
+        <div className="absolute z-10 top-0 right-0 bg-secondary w-18.5 h-18.5 rounded-full font-heading font-bold text-2xl flex justify-center items-center rotate-18 border-3 border-card">#0{position}</div>
 
-        <img src={image} alt={imageAlt} className="rounded-full w-74 h-74" />
+        <div className="absolute inset-0 border-2 border-primary rounded-full lg:hover:border-dashed lg:hover:animate-ring-spin transition-all duration-450"></div>
+
+        <img src={image} alt={imageAlt} className="rounded-full w-full p-3" />
       </div>
 
       <div className="text-center mt-3 rotate-2">
-        <p className="font-heading font-bold text-primary text-3xl mb-1">{favlorName}</p>
-        <p className="text-secondary text-lg font-extrabold uppercase">{description}</p>
+        <p className="font-heading font-bold text-primary text-3xl mb-1 md:text-2xl lg:text-3xl">{favlorName}</p>
+        <p className="text-secondary text-xl md:text-base lg:text-xl font-bold uppercase">{description}</p>
       </div>
     </div>
   );
--- src/pages/home/sections/top-3.tsx
+++ src/pages/home/sections/top-3.tsx
@@ -5,7 +5,7 @@ import { Top3Circle } from "@/components/common/top-3-circle";
 
 export function Top3() {
   return (
-    <section className="px-10 pt-16 md:px-11 lg:px-12 bg-background pb-20">
+    <section className="px-4 pt-25 pb-40 lg:px-12 bg-quinary">
       <div className="max-w-7xl mx-auto">
         <div className="flex items-center gap-4 mx-auto justify-center">
           <div className="h-px w-7 bg-primary"></div>
@@ -19,14 +19,14 @@ export function Top3() {
 
         <p className="pt-5 text-quaternary font-medium text-center lg:text-lg">Os sabores que roubaram o coração dos clientes — e não devolvem.</p>
 
-        <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-14 lg:flex-row lg:items-end">
+        <div className="pt-20 grid grid-cols-1 gap-15 md:grid-cols-3 md:place-items-center">
           <Top3Circle
             position={1} 
             favlorName="Nutinho c/ morango"
             description="o mais pedido"
             image={coneNutinhoMorango} 
             imageAlt="Cone de nutinho de morango"
-            className="order-1 sm:col-span-2 lg:col-span-1 lg:order-2"
+            className="order-1 md:order-2 md:translate-y-12"
           />
 
           <Top3Circle
@@ -35,16 +35,16 @@ export function Top3() {
             description="crocância máxima"
             image={coneOvomaltine}
             imageAlt="Cone de ovomaltine"
-            className="order-2 lg:order-1 lg:top-5"
+            className="order-2 md:order-1 md:top-5 md:-translate-y-12 w-[90%]"
           />
-
+ 
           <Top3Circle 
             position={3}
             favlorName="Oreo"
             description="cremosidade absurda"
             image={coneOreo}
             imageAlt="Cone de Oreo"
-            className="order-3 lg:top-5"
+            className="order-3 md:top-5 md:-translate-y-22 w-[82%]"
           />
         </div>
 
