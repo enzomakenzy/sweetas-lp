43da8dda12c871482d056b501fcf897bdf0201d7
enzomakenzy <enzomqueiroz@gmail.com>
2026-09-21 16:10:50 -0300

    style: adjust banner paddings and tweak quinary background color

--- src/pages/home/sections/banner.tsx
+++ src/pages/home/sections/banner.tsx
@@ -4,7 +4,7 @@ import bannerImg from "@/assets/images/cone-nutinho-morango.png";
 
 export function Banner() {
   return (
-    <section className="px-3 pt-4 pb-6 lg:pt-10 md:px-11 lg:px-12 lg:grid-cols-2 lg:gap-6 lg:items-center">
+    <section className="px-4 pt-4 pb-6 lg:pt-10 lg:px-12 lg:grid-cols-2 lg:gap-6 lg:items-center">
       <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 lg:gap-6 lg:items-center">
         <div className="flex flex-col gap-8 mb-8">
           <div>
@@ -28,7 +28,7 @@ export function Banner() {
         </div>
 
         <div className="pr-3 lg:pr-0">
-          <div className="relative shadow-lg rounded-xl sm:max-w-170 m-auto">
+          <div className="relative shadow-lg rounded-xl sm:max-w-160 m-auto">
             <img src={bannerImg} alt="Cone da Sweet a's" className="rounded-2xl w-full" />
 
             <p className="bg-primary absolute z-10 -right-2 -bottom-5 flex flex-col items-center justify-center rounded-full h-23 w-23 text-background text-2xl font-bold rotate-14 lg:text-3xl lg:h-27 lg:w-27">
