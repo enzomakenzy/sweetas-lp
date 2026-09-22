6177462040270731267c4bc93fdf841f97a386f6
enzomakenzy <enzomqueiroz@gmail.com>
2026-09-22 17:39:52 -0300

    feat: add responsive instagram feed section and fix wave svgs

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,6 +6,7 @@ import { Quality } from "@/pages/home/sections/quality";
 import { Reviews } from "@/pages/home/sections/reviews";
 import { Contact } from "@/pages/home/sections/contact";
 import TextLoop from "@/components/text-loop/TextLoop";
+import { Social } from "./sections/social";
 
 export default function Home() {
   return (
@@ -15,23 +16,24 @@ export default function Home() {
         <Marquee />
         <Top3 />
         <div className="w-full overflow-hidden h-20">
-          <svg viewBox="0 0 1200 130" preserveAspectRatio="none" className="w-[200%] h-20 md:h-24 animate-wave-inverse rotate-180">
-            <path d="M0,60 C200,130 400,0 600,60 C800,130 1000,0 1300,60 L1300,130 L0,130 Z" className="fill-quinary" />
+          <svg viewBox="0 0 1200 120" preserveAspectRatio="none" className="w-[200%] h-16 md:h-24 animate-wave-inverse rotate-180">
+            <path d="M0,60 C200,120 400,0 600,60 C800,120 1000,0 1200,60 L1200,120 L0,120 Z" className="fill-quinary" />
           </svg>
         </div>
         <Menu />
         <div className="w-full overflow-hidden h-20">
-          <svg viewBox="0 0 1200 130" preserveAspectRatio="none" className="w-[200%] h-20 md:h-24 animate-wave">
-            <path d="M0,60 C200,130 400,0 600,60 C800,130 1000,0 1300,60 L1300,130 L0,130 Z" className="fill-primary" />
+          <svg viewBox="0 0 1200 120" preserveAspectRatio="none" className="w-[200%] h-20 md:h-24 animate-wave">
+            <path d="M0,60 C200,120 400,0 600,60 C800,120 1000,0 1200,60 L1200,120 L0,120 Z" className="fill-primary" />
           </svg>
         </div>
         <Quality />
-        <div className="w-full overflow-hidden h-20">
-          <svg viewBox="0 0 1200 130" preserveAspectRatio="none" className="w-[200%] h-20 md:h-24 animate-wave-inverse rotate-180">
-            <path d="M0,60 C200,130 400,0 600,60 C800,130 1000,0 1300,60 L1300,130 L0,130 Z" className="fill-primary" />
+        <div className="w-full overflow-hidden h-20 bg-quinary">
+          <svg viewBox="0 0 1200 120" preserveAspectRatio="none" className="w-[200%] h-16 md:h-24 animate-wave-inverse rotate-180">
+            <path d="M0,60 C200,120 400,0 600,60 C800,120 1000,0 1200,60 L1200,120 L0,120 Z" className="fill-primary" />
           </svg>
         </div>
         <Reviews />
+        <Social />
         <TextLoop
           text="Mais Chocolate Por Favor ✦ Bateu Vontade? ✦ Sweet A's ✦ Peça o seu"
           shape="wave"
