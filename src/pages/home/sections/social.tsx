4f3529a1c43608f8154237b94171d669942e95ba
enzomakenzy <enzomqueiroz@gmail.com>
2026-09-25 11:07:39 -0300

    style: remove hover interaction from carousel images

--- src/pages/home/sections/social.tsx
+++ src/pages/home/sections/social.tsx
@@ -17,7 +17,7 @@ export function Social() {
             <p className="text-sm uppercase text-background font-bold">sweetasdoceria</p>
           </a>
 
-          <div className="w-full *:transition-all *:duration-350 flex overflow-x-auto py-4 gap-12 snap-x snap-mandatory *:w-72 *:rounded-2xl *:ring-2 *:ring-background *:snap-center md:snap-none *:shrink-0 scrollbar-none md:overflow-visible md:justify-center md:*:w-auto md:*:shrink md:*:flex-1 md:*:min-w-0 md:gap-16 *:backface-hidden *:will-change-transform *:hover:ring-4">
+          <div className="w-full *:transition-all *:duration-250 flex overflow-x-auto py-4 gap-12 snap-x snap-mandatory *:w-72 *:rounded-2xl *:ring-2 *:ring-background *:snap-center md:snap-none *:shrink-0 scrollbar-none md:overflow-visible md:justify-center md:*:w-auto md:*:shrink md:*:flex-1 md:*:min-w-0 md:gap-16 *:backface-hidden *:will-change-transform">
 
             <img src={photo1} alt="Foto 1" className="rotate-4 ml-8" />
             <img src={photo2} alt="Foto 2" className="-rotate-4" />
